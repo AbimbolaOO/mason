3da3c86f884c37730efa3aace13ab470c1ebebbe
Abimbola Olayemi <yemi@roadpreppers.com>
2025-01-22 12:16:25 +0100

    clean up

--- app/(website)/(landing-page)/ReachOutToUs/ReachOutToUs.tsx
+++ app/(website)/(landing-page)/ReachOutToUs/ReachOutToUs.tsx
@@ -6,39 +6,61 @@ import ArrowDownIcon from '@/components/Icons/ArrowDownIcon';
 
 const ReachOutToUs = () => {
   return (
-    <div className='border border-red-600 h-[100vh] bg-black'>
-      {/* paper bottom */}
-      <div className='h-[140px] relative border border-green-700 grid place-content-center -mt-[70px]'>
+    <div className='grid h-fit w-full *:col-start-1 *:row-start-1'>
+      {/* Foreground */}
+      <div className=' w-full relative'>
+        {/* <video
+          className='left-0 top-0 h-full w-full object-cover'
+          autoPlay
+          muted
+          loop
+        >
+          <source src='/static/video/reach-out-bg.mov' type='video/mp4' />
+          Your browser does not support the video tag.
+        </video> */}
         <Image
-          src='/static/img/bottom-paper-strip.png'
-          alt='paper strip'
+          src='/static/gif/reach-out-bg.gif'
+          alt='background image'
           fill
-          priority
-          className='object-cover object-center'
+          className='object-center'
         />
       </div>
 
-      {/* main content */}
-      <div className='w-full h-full border border-purple-700 grid place-content-center gap-[177px]'>
-        <div className='flex justify-center items-center flex-col border border-red-500'>
-          <div className='text-[35px] leading-[41px] text-white'>
-            Ready to bring your projects to life?
-          </div>
-          <div className='font-medium font-adventPro text-[100px] leading-[150px] text-white'>
-            <Link href='contact-us'>Reach Out to Us!</Link>
-            <div className='h-2 w-full origin-left bg-white rounded-full -mt-6'></div>
-          </div>
+      {/* Background */}
+      <div className='z-10 border border-red-600 h-[100vh]'>
+        {/* paper bottom */}
+        <div className='h-[140px] relative border border-red-700 grid place-content-center -mt-[70px]'>
+          <Image
+            src='/static/img/bottom-paper-strip.png'
+            alt='paper strip'
+            fill
+            priority
+            className='object-cover object-center'
+          />
         </div>
 
-        <button className='text-mason-black bg-white rounded-xl text-[15px] leading-[20px] p-[17px] w-fit justify-self-center flex gap-2 items-center'>
-          <div className='py-2 px-[10px]'>
-            <ArrowDownIcon />
+        {/* main content */}
+        <div className='w-full h-full border border-purple-700 grid place-content-center gap-[177px]'>
+          <div className='flex justify-center items-center flex-col border border-red-500'>
+            <div className='text-[35px] leading-[41px] text-white'>
+              Ready to bring your projects to life?
+            </div>
+            <div className='font-medium font-adventPro text-[100px] leading-[150px] text-white'>
+              <Link href='contact-us'>Reach Out to Us!</Link>
+              <div className='h-2 w-full origin-left bg-white rounded-full -mt-6'></div>
+            </div>
           </div>
-          Continue to Scroll
-          <div className='py-2 px-[10px]'>
-            <ArrowDownIcon />
-          </div>
-        </button>
+
+          <button className='text-mason-black bg-white rounded-xl text-[15px] leading-[20px] p-[17px] w-fit justify-self-center flex gap-2 items-center'>
+            <div className='py-2 px-[10px]'>
+              <ArrowDownIcon />
+            </div>
+            Continue to Scroll
+            <div className='py-2 px-[10px]'>
+              <ArrowDownIcon />
+            </div>
+          </button>
+        </div>
       </div>
     </div>
   );
--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
+'use client';
+import gsap from 'gsap';
+import React, { useRef } from 'react';
+
+import { useGSAP } from '@gsap/react';
 
 import DarumIcon from '../Icons/DarumIcon';
 import DwellingIcon from '../Icons/DwellingIcon';
 import EthiopianIcon from '../Icons/EthiopianIcon';
 
+gsap.registerPlugin(useGSAP);
+
 const Carousel = () => {
+  const container = useRef<HTMLDivElement>(null);
+  useGSAP(
+    (context, contextSafe) => {
+      if (!contextSafe) return;
+      const element = document.querySelector('.slide') as HTMLElement;
+      const elWidth = element ? element.offsetWidth : 0;
+
+      const slideIn = -elWidth + 3;
+      const slideOut = -(elWidth * 2);
+
+      gsap
+        .timeline({
+          duration: 1,
+          repeat: -1,
+        })
+        .to('.slide', {
+          x: slideIn,
+          stagger: {
+            each: 1,
+          },
+        })
+        .to(
+          '.slide',
+          {
+            x: slideOut,
+            opacity: 0,
+            stagger: {
+              each: 1,
+            },
+          },
+          '2'
+        );
+    },
+    { scope: container }
+  );
+
   return (
-    <div className='h-[470px] bg-mason-black rounded-[20px] grid text-white *:col-start-1 *:row-start-1'>
-      <div className='border border-red-700 flex overflow-auto'>
-        <div className='grid h-full grid-flow-col pb-[60px]'>
-          <div className='w-fit border border-yellow-500 grid place-content-center'>
-            <EthiopianIcon />
-          </div>
-          <div className='w-[600px] border border-green-700 grid place-content-center'>
-            <DwellingIcon />
-          </div>
-          <div className='w-[600px] border border-blue-700 grid place-content-center'>
-            <DarumIcon />
-          </div>
+    <div
+      ref={container}
+      className='h-[470px] bg-mason-black rounded-[20px] grid text-white *:col-start-1 *:row-start-1'
+    >
+      <div className='slide-container text-white w-[100%] my-8 mb-10 grid relative overflow-hidden'>
+        <div className='slide w-[calc(50vw-80px)] h-full grid place-content-center absolute -right-[calc(50vw-80px)]'>
+          <EthiopianIcon />
+        </div>
+        <div className='slide w-[calc(50vw-80px)] h-full grid place-content-center absolute -right-[calc(50vw-80px)]'>
+          <DwellingIcon />
+        </div>
+        <div className='slide w-[calc(50vw-80px)] h-full grid place-content-center absolute -right-[calc(50vw-80px)]'>
+          <DarumIcon />
         </div>
       </div>
 
