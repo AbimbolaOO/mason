7798acd56f30dcb685f13f3b24000bc7935a09f0
Abimbola Olayemi <yemi@roadpreppers.com>
2025-02-06 18:56:10 +0100

    update

--- app/(website)/book-a-free-consultation/page.tsx
+++ app/(website)/book-a-free-consultation/page.tsx
@@ -4,7 +4,7 @@ import ContactForm from '@/components/ContactForm/ContactForm';
 
 export default function page() {
   return (
-    <div>
+    <div className='h-screen flex max-md:mt-[60px] '>
       <ContactForm />
     </div>
   );
